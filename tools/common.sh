f47b8798ed8e6b113cd470769a83311dca0a5e50
Christoph Liebender <christoph.liebender@uni-wuerzburg.de>
2024-08-14 10:42:11 +0200

    add weight download

--- tools/common.sh
+++ tools/common.sh
@@ -1,9 +1,21 @@
 # https://stackoverflow.com/a/39087286
 gdrive_download() {
-        FILEID=$1
-        FILENAME=$2
+        local FILEID=$1
+        local FILENAME=$2
 	gdown "https://drive.google.com/uc?id=$FILEID" -O $FILENAME
         # wget -q --show-progress --load-cookies /tmp/__cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/__cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/__cookies.txt
+}
+
+http_download() {
+	local URL=$1
+	if command -v wcurl &> /dev/null; then
+		wcurl $URL
+	elif command -v wget &> /dev/null; then
+		wget $URL
+	else
+		echo 'neither wcurl nor wget exist in $PATH'
+		exit 1
+	fi
 }
 
 ensure_gum() {
--- tools/download_weights.sh
+++ tools/download_weights.sh
@@ -10,27 +10,35 @@ echo 'Choose architecture configuration'
 CONFIG=$(gum choose '(256,2)' '(128,2)' '(256,1)' '(256,2) - compat' '(128,2) - compat')
 
 mkdir -p weights
+cd weights
+
+BASEURL="https://robotik.informatik.uni-wuerzburg.de/telematics/exchange/anon-compression"
 
 case "$CONFIG" in
 	'(256,2)')
 		echo 'Width 256, Depth 2'
-#		gdrive_download FIXME weights/256-2.pt
+		http_download "$BASEURL/256-2.pt"
 		;;
 	'(128,2)')
 		echo 'Width 128, Depth 2'
-#		gdrive_download FIXME weights/128-2.pt
+		http_download "$BASEURL/128-2.pt"
 		;;
 	'(256,1)')
 		echo 'Width 256, Depth 1'
-#		gdrive_download FIXME weights/256-1.pt
+		http_download "$BASEURL/256-1.pt"
 		;;
 	'(256,2) - compat')
 		echo 'Width 256, Depth 1, jetson nano compatibility'
-#		gdrive_download FIXME weights/256-2-nanocompat.pt
+		http_download "$BASEURL/256-2-nanocompat.pt"
 		;;
 	'(128,2) - compat')
 		echo 'Width 128, Depth 2, jetson nano compatibility'
-#		gdrive_download FIXME weights/128-2-nanocompat.pt
+		http_download "$BASEURL/128-2-nanocompat.pt"
 		;;
 esac
 
+if ! [ -f 'sha256sums.txt' ]; then
+	http_download "$BASEURL/sha256sums.txt"
+fi
+
+sha256sum --ignore-missing --check sha256sums.txt
